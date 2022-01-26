9df6f5e4058443bf2746adcdb7198195f1a8ae47
Yoni Isaacs <yoni@hyro.ai>
2022-01-26 12:07:22 +0200

    improve client

--- agenda/examples/client_for_examples/client.js
+++ agenda/examples/client_for_examples/client.js
@@ -6,24 +6,24 @@ const socket = new WebSocket(`${"ws://0.0.0.0:9000"}/converse`);
 
 // Listen for messages
 socket.addEventListener("message", function (event) {
-  someArray.push("Bot utterance: " + event.data);
-  render()
+  someArray.push("Bot: " + event.data.replace(/(^"|"$)/g, ""));
+  render();
 });
 
-const someArray = []
+const someArray = [];
 
 prompt.start();
 const start = async () => {
   while (true) {
-    const {userInput} = await prompt.get(["userInput"]);
-    someArray.push("User utterance: " + userInput);
-    render()
+    const { userInput } = await prompt.get(["userInput"]);
+    someArray.push("User: " + userInput);
+    render();
     await socket.send(JSON.stringify(userInput));
   }
 };
 
 const render = () => {
-  console.clear()
-  console.log(someArray.join('\n'))
-}
+  console.clear();
+  console.log(someArray.join("\n"));
+};
 start();
