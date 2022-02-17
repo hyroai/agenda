6c3f24e79a43bc5981e978f4f191f7b1f63a68ee
Uri <uri.valevski@gmail.com>
2022-02-17 19:53:18 +0200

    style-fix

--- config_to_bot/debugger/src/App.js
+++ config_to_bot/debugger/src/App.js
@@ -1,29 +1,37 @@
-import { useReducer, useState, useEffect, useRef } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
 
-const rowSpacing = {display: "flex", flexDirection: "column", gap: 10};
+const rowSpacing = { display: "flex", flexDirection: "column", gap: 10 };
 
 const renderDebugState = ([key, value], i) => (
   <div key={i} marginRight={1}>
     <span>{key}: </span>
-    <span style={{color: "red"}}>{JSON.stringify(value).replace("null", "?")}</span>
+    <span style={{ color: "red" }}>
+      {JSON.stringify(value).replace("null", "?")}
+    </span>
   </div>
 );
 
 const renderDebugStates = (state) =>
-  state && Object.keys(state).length != 0 ? (
-      <div>{Object.entries(state).map(renderDebugState)}</div>
-    ) : null
-
+  state && Object.keys(state).length !== 0 ? (
+    <div>{Object.entries(state).map(renderDebugState)}</div>
+  ) : null;
 
 const textToBotUtterance = ({ botUtterance, state }) => (
-  <div style={{flexDirection: "row", justifyContent: "flex-start", gap:20, display: "flex"}}>
-    <div style={{color:"white"}}>🤖 {botUtterance}</div>
+  <div
+    style={{
+      flexDirection: "row",
+      justifyContent: "flex-start",
+      gap: 20,
+      display: "flex",
+    }}
+  >
+    <div style={{ color: "white" }}>🤖 {botUtterance}</div>
     {renderDebugStates(state)}
   </div>
 );
 const textToUsertUtterance = ({ userUtterance }) => (
-  <span style={{color:"lightblue"}}>👩 {userUtterance}</span>
+  <span style={{ color: "lightblue" }}>👩 {userUtterance}</span>
 );
 const renderEvent = (event, i) => (
   <span marginTop={1} key={i}>
@@ -45,22 +53,15 @@ const App = () => {
     []
   );
   const [textInput, setTextInput] = useState("");
-  const [socket, setSocket] = useState(null);
   const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
     "ws://0.0.0.0:9000/converse",
     {
-      shouldReconnect: (closeEvent) => {
-        return didUnmount.current === false;
-      },
+      shouldReconnect: () => didUnmount.current === false,
       reconnectAttempts: 100,
       reconnectInterval: 3000,
     }
   );
-  useEffect(() => {
-    return () => {
-      didUnmount.current = true;
-    };
-  }, []);
+  useEffect(() => () => (didUnmount.current = true), []);
 
   useEffect(() => {
     if (lastJsonMessage !== null) {
@@ -78,7 +79,8 @@ const App = () => {
         [ReadyState.UNINSTANTIATED]: "Uninstantiated",
       }[readyState]
     );
-    if (readyState === ReadyState.CONNECTING) addEvent({ userUtterance: "reset" });
+    if (readyState === ReadyState.CONNECTING)
+      addEvent({ userUtterance: "reset" });
   }, [readyState, addEvent]);
 
   const connectionStatus = {
@@ -96,9 +98,13 @@ const App = () => {
           <div style={rowSpacing}>
             <div style={rowSpacing}>{events.map(renderEvent)}</div>
             <div>
-              >{" "}
+              {"> "}
               <input
-                style={{background:"transparent", color:"white"}}
+                style={{
+                  fontFamily: "monospace",
+                  background: "transparent",
+                  color: "white",
+                }}
                 autoFocus={true}
                 type="text"
                 value={textInput}
