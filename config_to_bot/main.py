8d9210cb42cedd973f0f096634543638f546d50d
Yoni Isaacs <yoni@hyro.ai>
2022-02-20 13:56:21 +0200

    remove init file

--- config_to_bot/main.py
+++ config_to_bot/main.py
@@ -10,8 +10,8 @@ from computation_graph import graph
 from starlette import websockets
 from uvicorn.main import Server
 
-import config_to_bot
 from agenda import composers
+from config_to_bot import resolvers, yaml_to_bot
 
 
 def _error_message(error):
@@ -21,13 +21,13 @@ def _error_message(error):
 def _create_socket_handler(path: str):
     async def message_handler(websocket: fastapi.WebSocket):
         state: dict = {}
-        bot = config_to_bot.yaml_to_slot_bot(path)()
+        bot = yaml_to_bot.yaml_to_slot_bot(path)()
 
         async def responder_with_state(request):
             nonlocal state
             nonlocal bot
             if request.lower() == "reload":
-                bot = config_to_bot.yaml_to_slot_bot(path)()
+                bot = yaml_to_bot.yaml_to_slot_bot(path)()
                 state = {}
                 return {"botUtterance": "Reloading bot"}
             if request.lower() == "reset":
@@ -38,7 +38,7 @@ def _create_socket_handler(path: str):
                 return {
                     "botUtterance": state[graph.make_computation_node(composers.utter)],
                     "state": gamla.pipe(
-                        graph.make_computation_node(config_to_bot.debug_states),
+                        graph.make_computation_node(resolvers.debug_states),
                         gamla.dict_to_getter_with_default({}, state),
                         gamla.valmap(
                             gamla.valmap(
