026100687020a0e1c0d932f6bd7adce402318b24
Yoni Isaacs <yoni@hyro.ai>
2022-02-24 14:41:09 +0200

    adding more tests

--- config_to_bot/bot_test.py
+++ config_to_bot/bot_test.py
@@ -18,14 +18,22 @@ async def _remote_function(url: str, params: Dict):
 
     def topping_render(toppings: Tuple[str, ...]):
         if len(toppings) == 1:
-            return f"with {toppings[0]}"
+            return f" with {toppings[0]}"
         if len(toppings) > 1:
             str_of_toppings = ", ".join(toppings[:-1])
-            return f"with {str_of_toppings}, and {toppings[-1]}"
+            return f" with {str_of_toppings}, and {toppings[-1]}"
         return ""
 
-    if name and amount_of_pizzas and toppings and size and address and phone and email:
-        return f"Thank you {name}! I got your phone: {phone}, and your email: {email}. We are sending you {int(amount_of_pizzas)} {size} pizzas {topping_render(toppings)} to {address}."
+    if (
+        name
+        and amount_of_pizzas
+        and size
+        and address
+        and phone
+        and email
+        and toppings is not None
+    ):
+        return f"Thank you {name}! I got your phone: {phone}, and your email: {email}. We are sending you {int(amount_of_pizzas)} {size} pizzas{topping_render(toppings)} to {address}."
     return None
 
 
@@ -72,4 +80,47 @@ async def test_skip_toppings_question():
     ]
 )
 async def test_happy_flow():
+    return pizza_bot
+
+
+@agenda.expect_convos(
+    [
+        [
+            ["hi", "Would you like to order pizza?"],
+            ["yes, I want pizza", "Got it. Are you vegan?"],
+            ["no", "Got it. What is your name?"],
+            ["Yoni", "Got it. How many pies would you like?"],
+            ["4", "Got it. What kind of toppings would you like?"],
+            ["none", "Got it. What pizza size would you like?"],
+            ["small", "Got it. What is your address?"],
+            ["881 Mill Street Greenville SC", "Got it. What is your phone number?"],
+            ["999888777", "Got it. What is your email?"],
+            [
+                "abcd1234@gmail.com",
+                "Got it. Thank you Yoni! I got your phone: 999888777, and your email: abcd1234@gmail.com. We are sending you 4 small pizzas to 881 Mill Street Greenville SC.",
+            ],
+        ]
+    ]
+)
+async def test_happy_flow_without_toppings():
+    return pizza_bot
+
+
+@agenda.expect_convos(
+    [
+        [
+            ["hi", "Would you like to order pizza?"],
+            ["yes, I want 2 large pizzas with mushrooms", "Got it. Are you vegan?"],
+            ["no", "Got it. What is your name?"],
+            ["Yoni", "Got it. What is your address?"],
+            ["881 Mill Street Greenville SC", "Got it. What is your phone number?"],
+            ["999888777", "Got it. What is your email?"],
+            [
+                "abcd1234@gmail.com",
+                "Got it. Thank you Yoni! I got your phone: 999888777, and your email: abcd1234@gmail.com. We are sending you 2 large pizzas with mushrooms to 881 Mill Street Greenville SC.",
+            ],
+        ]
+    ]
+)
+async def test_happy_flow_wihtout_asking_for_amount_and_size():
     return pizza_bot
