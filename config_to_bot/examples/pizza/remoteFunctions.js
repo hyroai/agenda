9b0df57d89cb0c9eff65b6b4d02eafe462a99d23
Uri <uri.valevski@gmail.com>
2022-03-16 17:16:06 +0200

    improve-example-readability

--- config_to_bot/examples/pizza/remoteFunctions.js
+++ config_to_bot/examples/pizza/remoteFunctions.js
@@ -1,43 +1,44 @@
 const express = require("express");
+
 const app = express();
-const renderToppings = (toppings) => {
-  if (toppings.length == 1) {
-    return `with ${toppings}`;
-  }
-  if (toppings.length > 1) {
-    return `with ${toppings.slice(0, -1).join(", ")}, and ${
-      toppings[toppings.length - 1]
-    }`;
-  } else {
-    return ``;
-  }
-};
+
+const renderToppings = (toppings) =>
+  toppings.length == 1
+    ? `with ${toppings}`
+    : toppings.length > 1
+    ? `with ${toppings.slice(0, -1).join(", ")}, and ${
+        toppings[toppings.length - 1]
+      }`
+    : "";
+
 app.use(express.json());
-app.post(
-  "/order-pizza",
-  (
-    { body: { phone, email, amount_of_pizzas, name, address, size, toppings } },
-    res
-  ) => {
-    console.log("Got a POST request for /order-pizza");
-    if (
-      phone &&
-      email &&
-      amount_of_pizzas &&
-      name &&
-      address &&
-      size &&
-      toppings
-    ) {
-      res.json(
-        `Thank you ${name}! I got your phone: ${phone}, and your email: ${email}. We are sending you ${amount_of_pizzas} ${size} pizzas ${renderToppings(
-          toppings
-        )} to ${address}.`
-      );
-    } else {
-      res.json(null);
-    }
-  }
-);
+
+const validateRequest = ({
+  phone,
+  email,
+  amount_of_pizzas,
+  name,
+  address,
+  size,
+  toppings,
+}) => phone && email && amount_of_pizzas && name && address && size && toppings;
+
+const renderSuccess = ({
+  name,
+  phone,
+  email,
+  address,
+  amount_of_pizzas,
+  size,
+}) =>
+  `Thank you ${name}! I got your phone: ${phone}, and your email: ${email}. We are sending you ${amount_of_pizzas} ${size} pizzas ${renderToppings(
+    toppings
+  )} to ${address}.`;
+
+app.post("/order-pizza", ({ body }, res) => {
+  res.json(validateRequest(body) ? renderSuccess : null);
+});
+
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Example app listening at post ${port}`));
+
+app.listen(port, () => console.log(`app listening at post ${port}`));
